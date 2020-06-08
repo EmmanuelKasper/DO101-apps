a1edda4847fbe6d3522e73b4174d3a489c0253d1
Emmanuel Kasper <manu@debian.org>
2020-06-08 16:09:26 +0200

    Add mars routing end point and arrow function

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+app.get('/', (req, res) => res.send('Hello World!\n'));
+app.get('/mars', (req, res) => res.send('Hello Mars\n'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
