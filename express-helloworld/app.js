b120d6fb4a056d527f4160811f528b0f1f0503d5
Emmanuel Kasper <manu@debian.org>
2020-07-21 14:40:46 +0200

    Brabe new lambda

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+app.get('/', (req, res) => res.send('Hello Brave New World!\n'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
